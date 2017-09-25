130cf39a569101e134d7fd812c58ab68f4b2bab9
a172586418 <172586418@qq.com>
2017-09-25 22:02:49 +0800

    HW.MusicStore 完善BaseDao中的基础接口。

--- CSharp/HW.MusicStore/HW.MusicStore.Dao/BaseDao.cs
+++ CSharp/HW.MusicStore/HW.MusicStore.Dao/BaseDao.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Data.SQLite;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -94,6 +95,22 @@ namespace HW.MusicStore.Dao
             return local;
         }
 
+        protected virtual Hashtable GetHashByEntity(T entity)
+        {
+            Hashtable hashtable = new Hashtable();
+            PropertyInfo[] propertyInfos = entity.GetType().GetProperties();
+            for (int i = 0; i < propertyInfos.Length; i++)
+            {
+                object obj = propertyInfos[i].GetValue(entity, null);
+                obj = obj ?? DBNull.Value;
+                if (!hashtable.ContainsKey(propertyInfos[i].Name))
+                {
+                    hashtable.Add(propertyInfos[i].Name, obj);
+                }
+            }
+            return hashtable;
+        }
+
         protected virtual Hashtable GetHashEntity(T entity)
         {
             Hashtable hashtable = new Hashtable();
@@ -108,6 +125,22 @@ namespace HW.MusicStore.Dao
                 }
             }
             return hashtable;
+        }
+
+        private IEnumerable<T> GetList(string sql, IDbDataParameter[] parameters)
+        {
+            T item = default(T);
+            List<T> list = new List<T>();
+            InternalDbSession session = DataFactory.CreateDatabase();
+            using (IDataReader reader = session.ExecuteReader(sql, parameters))
+            {
+                while (reader.Read())
+                {
+                    item = this.DataReaderToEntity(reader);
+                    list.Add(item);
+                }
+            }
+            return list;
         }
 
         public bool Insert(T entity)
@@ -141,12 +174,15 @@ namespace HW.MusicStore.Dao
 
         public bool DeleteByKey(string key)
         {
-            throw new NotImplementedException();
+            string condition = string.Format("{0}=@ID", this.primaryKey);
+            SQLiteParameter parameter = new SQLiteParameter("@ID", key);
+            return this.DeleteByCondition(condition, null, new SQLiteParameter[] {parameter});
         }
 
         public bool DeleteByKey(string key, DbTransaction transaction)
         {
-            throw new NotImplementedException();
+            string condition = string.Format("{0}={1}", this.primaryKey, key);
+            return this.DeleteByCondition(condition, transaction);
         }
 
         public bool Update(T entity, string primaryKey)
@@ -171,27 +207,49 @@ namespace HW.MusicStore.Dao
 
         public IEnumerable<T> Find(string condition)
         {
-            throw new NotImplementedException();
+            string sql = string.Format("SELECT {0} FROM {1} WHERE {2} ORDER BY {3} {4}", this.selectedFields,
+                this.tableName, condition, this.sortField, this.isDescending ? "DESC" : "ASC");
+
+            return this.GetList(sql, null);
         }
 
         public IEnumerable<T> Find(string condition, IDbDataParameter[] parameters)
         {
-            throw new NotImplementedException();
+            string sql = string.Format("SELECT {0} FROM {1} WHERE {2} ORDER BY {3} {4}", this.selectedFields,
+                this.tableName, condition, this.sortField, this.isDescending ? "DESC" : "AES");
+            return this.GetList(sql, parameters);
         }
 
         public T FindById(int key)
         {
-            throw new NotImplementedException();
+            return this.FindById(key.ToString());
         }
 
         public T FindById(string key)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(key))
+            {
+                return default(T);
+            }
+            string query = string.Format("SELECT {0} FROM {1} WHERE {2} = @ID", this.selectedFields, this.tableName,
+                this.primaryKey);
+            IDbDataParameter parameter = new SQLiteParameter("@ID", key);
+            InternalDbSession session = DataFactory.CreateDatabase();
+            T local = default(T);
+            using (IDataReader reader = session.ExecuteReader(query, new IDbDataParameter[] {parameter}))
+            {
+                if (reader.Read())
+                {
+                    local = this.DataReaderToEntity(reader);
+                }
+            }
+            return local;
         }
 
         public IEnumerable<T> FindByIds(string ids)
         {
-            throw new NotImplementedException();
+            string condition = string.Format("{0} in ({1})", this.primaryKey, ids);
+            return Find(condition);
         }
 
         public T FindLast()
@@ -206,17 +264,31 @@ namespace HW.MusicStore.Dao
 
         public T FindSingle(string condition)
         {
-            throw new NotImplementedException();
+            T local = default(T);
+            IEnumerable<T> list = this.Find(condition);
+            if (list.Any())
+            {
+                local = list.FirstOrDefault();
+            }
+            return local;
         }
 
         public T FindSingle(string condition, IDbDataParameter[] parameters)
         {
-            throw new NotImplementedException();
+            T local = default(T);
+            IEnumerable<T> list = this.Find(condition, parameters);
+            if (list.Any())
+            {
+                local = list.FirstOrDefault();
+            }
+            return local;
         }
 
         public IEnumerable<T> FindAll()
         {
-            throw new NotImplementedException();
+            string sql = string.Format("SELECT {0} FROM {1} ORDER BY {2} {3}", this.selectedFields, this.tableName,
+                this.sortField, this.isDescending ? "DESC" : "AES");
+            return this.GetList(sql, null);
         }
 
         public int GetMaxId()
