a71bc5538e057cf473e12fe4262881ceb079649a
a172586418 <172586418@qq.com>
2017-11-10 22:14:55 +0800

    Angular案例，https://angular.cn/tutorial

--- AngularJS/angular2.0/App-store/src/app/app.component.ts
+++ AngularJS/angular2.0/App-store/src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  //templateUrl: './app.component.html',
+  template:`
+  <h1>{{title}}</h1>
+  <nav>
+    <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+    <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    <router-outlet></router-outlet>
+  </nav>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
