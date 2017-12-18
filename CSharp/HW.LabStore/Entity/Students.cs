8367d094555846e51d5c516593ea26d68f25b818
a172586418 <172586418@qq.com>
2017-12-18 22:11:51 +0800

    实验室管理系统，HW.LabStore.Entity

---
+++ CSharp/HW.LabStore/Entity/Students.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW.LabStore.Entity
+{
+    public class Students:BaseEntity
+    {
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string UserPwd { get; set; }
+
+        /// <summary>
+        /// 学生学号
+        /// </summary>
+        public string UserNum { get; set; }
+
+        /// <summary>
+        /// 所在年级
+        /// </summary>
+        public string Grade { get; set; }
+
+        /// <summary>
+        /// 所属专业
+        /// </summary>
+        public string Profession { get; set; }
+
+        /// <summary>
+        /// 所在课题组
+        /// </summary>
+        public string TopicGUID { get; set; }
+
+        /// <summary>
+        /// 所在实验室
+        /// </summary>
+        public string LabGUID { get; set; }
+
+        /// <summary>
+        /// 所在院系
+        /// </summary>
+        public string DepartGUID { get; set; }
+    }
+}
---
+++ CSharp/HW.LabStore/Entity/Teachers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HW.LabStore.Entity
+{
+    public class Teachers:BaseEntity
+    {
+        /// <summary>
+        /// 教师名称
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 登录密码
+        /// </summary>
+        public string UserPwd { get; set; }
+
+        /// <summary>
+        /// 教师编号
+        /// </summary>
+        public string UserNum { get; set; }
+
+        /// <summary>
+        /// 所在办公室
+        /// </summary>
+        public string Office { get; set; }
+
+        /// <summary>
+        /// 办公室电话
+        /// </summary>
+        public string OfficeTel { get; set; }
+
+        /// <summary>
+        /// 所在课题组
+        /// </summary>
+        public string TopicGUID { get; set; }
+
+        /// <summary>
+        /// 所属实验室
+        /// </summary>
+        public string LabGUID { get; set; }
+
+        /// <summary>
+        /// 所属院系
+        /// </summary>
+        public string DepartGUID { get; set; }
+    }
+}
