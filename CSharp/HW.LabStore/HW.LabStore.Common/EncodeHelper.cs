28eadd7e7d74ff50760aac00fdf843f6219febcf
a172586418 <172586418@qq.com>
2017-12-21 21:47:22 +0800

    实现登录页面。

---
+++ CSharp/HW.LabStore/HW.LabStore.Common/EncodeHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW.LabStore.Common
+{
+    public class EncodeHelper
+    {
+        private static readonly byte[] arrDESKey = new byte[] { 42, 16, 93, 156, 78, 4, 218, 32 };
+        private static readonly byte[] arrDESIV = new byte[] { 55, 103, 246, 79, 36, 99, 167, 3 };
+
+        /// <summary>
+        /// 加密。
+        /// </summary>
+        /// <param name="m_Need_Encode_String"></param>
+        /// <returns></returns>
+        public static string Encode(string m_Need_Encode_String)
+        {
+            DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+            MemoryStream objMemoryStream = new MemoryStream();
+            CryptoStream objCryptoStream = new CryptoStream(objMemoryStream, objDES.CreateEncryptor(arrDESKey, arrDESIV), CryptoStreamMode.Write);
+            StreamWriter objStreamWriter = new StreamWriter(objCryptoStream);
+            objStreamWriter.Write(m_Need_Encode_String);
+            objStreamWriter.Flush();
+            objCryptoStream.FlushFinalBlock();
+            objMemoryStream.Flush();
+            return Convert.ToBase64String(objMemoryStream.GetBuffer(), 0, (int)objMemoryStream.Length);
+        }
+
+        /// <summary>
+        /// 解密。
+        /// </summary>
+        /// <param name="m_Need_Encode_String"></param>
+        /// <returns></returns>
+        public static string Decode(string m_Need_Encode_String)
+        {
+            DESCryptoServiceProvider objDES = new DESCryptoServiceProvider();
+            byte[] arrInput = Convert.FromBase64String(m_Need_Encode_String);
+            MemoryStream objMemoryStream = new MemoryStream(arrInput);
+            CryptoStream objCryptoStream = new CryptoStream(objMemoryStream, objDES.CreateDecryptor(arrDESKey, arrDESIV), CryptoStreamMode.Read);
+            StreamReader objStreamReader = new StreamReader(objCryptoStream);
+            return objStreamReader.ReadToEnd();
+        }
+
+    }
+}
