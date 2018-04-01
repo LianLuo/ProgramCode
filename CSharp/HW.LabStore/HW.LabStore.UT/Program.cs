f3a0b35bf0c81e8e3d331432effedef21227dd2a
a172586418 <172586418@qq.com>
2018-04-01 16:25:36 +0800

    new add a module of UT

---
+++ CSharp/HW.LabStore/HW.LabStore.UT/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace HW.LabStore.UT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public class CourseData
+    {
+        public async Task AddCourseAsync()
+        {
+            var connection = new SqlConnection("");
+            var cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO Courses (Number,Title) VALUES (@number,@title)";
+            await connection.OpenAsync();
+            var tx = connection.BeginTransaction();
+            try
+            {
+                cmd.Transaction = tx;
+                cmd.Parameters.AddWithValue("", "");
+                cmd.Parameters.AddWithValue("", "");
+                await cmd.ExecuteNonQueryAsync();
+                tx.Commit();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                tx.Rollback();
+                throw;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
