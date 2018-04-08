a30af07b29fe5fcf3f01e26463b8605675b56b1a
a172586418 <172586418@qq.com>
2018-04-08 21:34:57 +0800

    try to add function.

--- CSharp/HW.LabStore/HW.LabStore.BusinessModel/PEPBusinessComponent.cs
+++ CSharp/HW.LabStore/HW.LabStore.BusinessModel/PEPBusinessComponent.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HW.LabStore.DbModel;
 
 namespace HW.LabStore.BusinessModel
 {
     public class PEPBusinessComponent
     {
+        private mallStoreEntities Context
+        {
+            get { return new mallStoreEntities();}
+        }
     }
 }
