9608fdf3b7ccbcffc2a2f54b85221d14e0cb564a
a172586418 <172586418@qq.com>
2019-03-03 20:45:57 +0800

    add some code.

--- CSharp/HW.LabStore/HW.LabStore.UI/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW.LabStore.UI.MvcApplication" Language="C#" %>
--- CSharp/HW.MusicStore/HW.MusicStore.UI/App_Data/MusicStore.sql
+++
@@ -1,5 +0,0 @@
-﻿select album.albumId, album.Title,genre.Name,artist.Name,album.price,album.url from album
-left join artist
-on [album].[ArtistId] = [Artist].[ArtistId]
-left join genre
-on album.GenreId = genre.GenreId
